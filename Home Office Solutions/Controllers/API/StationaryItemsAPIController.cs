5d3eab490855d6297cab647ce0712888ba337c71
Majella Haverty <X00168013@myTUDublin.ie>
2021-04-29 15:51:56 +0100

    API Controller

---
+++ Home Office Solutions/Controllers/API/StationaryItemsAPIController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Home_Office_Solutions.Models;
+
+namespace Home_Office_Solutions.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationaryItemsAPIController : ControllerBase
+    {
+        private ShopContext _context = new ShopContext();
+
+        // GET: api/StationaryItemsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StationaryItem>>> GetstationaryItems()
+        {
+            if (_context.stationaryItems.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                return await _context.stationaryItems.ToListAsync();  
+            }
+
+        }
+
+        private bool StationaryItemExists(int id)
+        {
+            return _context.stationaryItems.Any(e => e.ProductID == id);
+        }
+    }
+}
