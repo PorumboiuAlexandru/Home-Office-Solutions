a051a66c89c203110307be2ed6501d9af7ca4aa4
Alexandru Porumboiu <X00168014@myTUDublin.ie>
2021-05-04 21:54:42 +0100

    context

--- Home Office Solutions/Models/ShopContext.cs
+++ Home Office Solutions/Models/ShopContext.cs
@@ -10,7 +10,7 @@ namespace Home_Office_Solutions.Models
     public class ShopContext : DbContext
     {
         /*private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=HomeOfficeSolutions10;Trusted_Connection=False;";*/
-        private const string connectionString = "Server=tcp:homeofficesql.database.windows.net,1433;Initial Catalog=HomeOfficeSql;Persist Security Info=False;User ID=ad;Password=M@thes0n;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private const string connectionString = "Server=tcp:homeofficesql.database.windows.net,1433;Initial Catalog=HomeOfficeSql;Persist Security Info=False;User ID=ad;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
